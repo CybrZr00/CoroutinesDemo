54fe6c898c0095dba47efa8d7406e693ef974d69
Mark <markperry.uk@gmail.com>
2023-02-27 19:57:24 +0000

    Update UI from coroutine

--- app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -5,16 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var messageText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        messageText = findViewById(R.id.tvMessage)
         val textView = findViewById<TextView>(R.id.tvCount)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
@@ -29,10 +28,15 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-}
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                messageText.text = "Downloading user $i"
+            }
 
-private fun downloadUserData() {
-    for (i in 1..200000) {
-        Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            delay(100)
+        }
     }
 }
+
